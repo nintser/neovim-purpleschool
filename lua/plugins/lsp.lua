188f821d93e5fc74832f8ccef5d7cd04d336f99d
nintser <nintser@gmail.com>
2024-09-05 18:53:55 +0700

    Full course commit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
+			lspconfig.ts_ls.setup({})
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 				callback = function(ev)
